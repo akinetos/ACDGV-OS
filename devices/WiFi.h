969b1170b9dd8baa458df378b0828402a151bea8
akinetos <adam.nakoneczny@gmail.com>
2025-07-30 09:26:11 +0200

    IMP: WiFi

--- devices/WiFi.h
+++ devices/WiFi.h
@@ -6,14 +6,26 @@ class Wifi: public Device {
     int attempts = 0;
     const int maxAttempts = 20;
     const int delayTime = 1000;
+    String ip = "";
+
+    String IpAddress2String(const IPAddress& ip) {
+      char buffer[20];
+      sprintf(buffer, "%i.%i.%i.%i", ip[0], ip[1], ip[2], ip[3]);
+      return buffer;
+    }
 
     void init() {
       WiFi.mode(WIFI_STA);
       WiFi.begin(this->networkName, this->networkPassword);
+      
       while (!this->connected && this->attempts < this->maxAttempts) {
         delay(this->delayTime);
         this->connected = WiFi.status() == WL_CONNECTED;
         this->attempts++;
+      }
+      
+      if (this->connected) {
+        this->ip = this->IpAddress2String(WiFi.localIP());
       }
     }
   
--- programs/Router.h
+++ programs/Router.h
@@ -3,14 +3,8 @@ class Router:public Program {
       String networks[20];
       int networksCount = 0;
       int counter = 0;
-      String mode = "";
+      int option = -1;
       int startedTime;
-
-      String IpAddress2String(const IPAddress& ip) {
-        char buffer[20];
-        sprintf(buffer, "%i.%i.%i.%i", ip[0], ip[1], ip[2], ip[3]);
-        return buffer;
-      }
 
       void init() {
         this->startedTime = millis();
@@ -31,69 +25,10 @@ class Router:public Program {
         }
       }
 
-      void tick() {
-        /*
-        Surface & s2x1 = surfaces[0];
-        RE & re = channels[0].ports[5].re;
-        OLED & screen = channels[s2x1.channel].ports[1].screen;
-
-        if (s2x1.facingUp) {
-          if (this->mode == "status") {
-            screen.heading = "stat";
-            for (int i=0; i<this->networksCount; i++) {
-              screen.lines[i] = this->networks[i];
-            }
-            screen.optionsCount = this->networksCount;
-            screen.hasOptions = true;
-            screen.needsRefresh = true;
-          }
-          
-          if (this->mode == "networks") {
-            screen.heading = "netw";
-            for (int i=0; i<this->networksCount; i++) {
-              screen.lines[i] = this->networks[i];
-            }
-            screen.optionsCount = this->networksCount;
-            screen.hasOptions = true;
-            screen.needsRefresh = true;
-          }
-
-          if (this->mode == "networks" && re.button) {
-            this->mode = "network";
-            screen.heading = screen.lines[screen.lineHovered];
-            screen.hasOptions = false;
-          }
-
-          if (this->mode == "network") {
-            screen.printHeading(2);
-          }
-        }
-        
-        Surface & s8x1 = surfaces[1];
-        if (s8x1.facingUp) {
-          OLED & topScreen = channels[s8x1.channel].ports[0].screen;
-          topScreen.heading = "WiFi";
-          topScreen.printHeading(2);
-          for (int n=0; n<networksCount; n++) {
-            OLED & screenN = channels[s8x1.channel].ports[n+1].screen;
-            screenN.heading = this->networks[n];
-            screenN.hasOptions = false;
-            screenN.printHeading(1);
-          }
-        }
-        */
-      }
+      void tick() {}
 
       void setOption(int option) {
-        if (option == -1) {
-          this->mode = "";
-        }
-        if (option == 0) {
-          this->mode = "status";
-        }
-        if (option == 1) {
-          this->mode = "networks";
-        }
+        this->option = option;
       }
 
       void becameActive() {}
