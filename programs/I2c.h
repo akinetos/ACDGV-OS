2458a088be586e54c2523a2d52712e787b0059a5
akinetos <adam.nakoneczny@gmail.com>
2025-11-18 10:38:47 +0100

    poprawki w programie i2c

--- programs/I2c.h
+++ programs/I2c.h
@@ -1,9 +1,19 @@
+class item {
+  public:
+    String name;
+    int address[8];
+  item() {}
+};
+
 class I2c:public Program {
   public:
     String foundDevices[10];
     int foundAddresses[10];
     int count = 0;
     boolean scanned = false;
+    String stats = "";
+    item list[10];
+    int totalDevicesCount = 0;
 
     void init() {
       if (!this->scanned) {
@@ -12,19 +22,45 @@ class I2c:public Program {
     }
 
     void scan() {
-      JsonArray & controllers = loadFromFile("/config/sensors.json");
+      JsonArray & devices = loadFromFile("/config/devices.json");
+      int devicesCount = devices.size();
+      for (int d=0; d<devicesCount; d++) {
+        String name = devices[d]["name"];
+        this->list[d].name = name;
+        for (int a=0; a<devices[d]["address"].size(); a++) {
+          int address = devices[d]["address"][a];
+          this->list[d].address[a] = address;
+        }
+      }
+
+      JsonArray & sensors = loadFromFile("/config/sensors.json");
+      int sensorsCount = sensors.size();
+      for (int d=0; d<sensorsCount; d++) {
+        int index = d + devicesCount;
+        String name = sensors[d]["name"];
+        this->list[index].name = name;
+        for (int a=0; a<sensors[d]["address"].size(); a++) {
+          int address = sensors[d]["address"][a];
+          this->list[index].address[a] = address;
+        }
+      }
+
+      this->totalDevicesCount = devicesCount + sensorsCount;
+
       for (int address = 8; address < 120; address++) {
         Wire.beginTransmission(address);
         if (Wire.endTransmission() == 0) {
           String deviceType = "unknown";
-          for (int d=0; d<controllers.size(); d++) {
-            for (int a=0; a<controllers[d]["address"].size(); a++) {
-              if (controllers[d]["address"][a] == address) {
-                String name = controllers[d]["name"];
+          
+          for (int d=0; d<(devicesCount+sensorsCount); d++) {
+            for (int a=0; a<8; a++) {
+              if (this->list[d].address[a] == address) {
+                String name = this->list[d].name;
                 deviceType = name;
               }
             }
           }
+
           foundAddresses[count] = address;
           foundDevices[count] = deviceType;
           count++;
@@ -53,6 +89,12 @@ class I2c:public Program {
           screen.offsetY = 0;
           screen.printLines();
         }
+        OLED & statsScreen = channels[0].ports[7].screen;
+        statsScreen.hasOptions = true;
+        statsScreen.optionsCount = 1;
+        statsScreen.lines[0] = (String)this->totalDevicesCount;
+        statsScreen.lineSelected = -1;
+        statsScreen.printLines();
       } else {
         OLED & screen = channels[0].ports[1].screen;
         screen.hasOptions = true;
