afa4ba07f8e34cfd756b7c529f219ca39b6b4821
akinetos <adam.nakoneczny@gmail.com>
2026-08-06 23:56:58 +0200

    skaner 3d - poprawki

--- programs/Skaner3d.h
+++ programs/Skaner3d.h
@@ -1,21 +1,19 @@
 class Skaner3d:public Program {
   	public:
-		double distances[8];	
+		double distances[8];
 
 		void init() {
 			this->initialised = true;
 		}
 
 		void tick() {
-			for (int i=0; i<8; i++)
+			for (int i=0; i<8; i++) {
+				this->distances[i] = channels[0].ports[i].devices[0]->readNumber("distance");
 				channels[0].ports[i].screen.needsRefresh = true;
+			}
 		}
 		
 		void draw() {
-			for (int i=0; i<8; i++)
-				this->distances[i] = channels[0].ports[i].devices[0]->readNumber("distance");
-
-			
 			for (int i=0; i<8; i++)
 				for (int x=0; x<distances[i]; x++)
 					channels[0].ports[i].screen.ssd1306.drawLine(x,0,x,31,SSD1306_WHITE);
